ac11299e7ac5d6fbe10538193ed5eeb5e558ea7d
Addo.Zhang <duwasai@gmail.com>
2023-12-06 20:50:40 +0800

    add otel support

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 const (
@@ -13,50 +18,111 @@ const (
 	App            = "app"
 	Version        = "version"
 	Upstream       = "upstream"
+	Port           = "port"
 )
 
+var upstream = os.Getenv(Upstream)
+var appName = os.Getenv(App)
+var version = os.Getenv(Version)
+var port = os.Getenv(Port)
+
 func main() {
-	port := os.Getenv("port")
 	if port == "" {
 		port = "8080"
 	}
 
-	upstream := os.Getenv(Upstream)
-	appName := os.Getenv(App)
-	version := os.Getenv(Version)
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ip, hostname := getIPAndHostname()
-		response := fmt.Sprintf("%s(version: %s, ip: %s, hostname: %s)", appName, version, ip, hostname)
+	// Set up OpenTelemetry.
+	otelShutdown, err := setupOTelSDK(ctx, appName, version)
 
-		if upstream != "" {
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
 
-			client := &http.Client{}
-			req, _ := http.NewRequest("GET", upstream, nil)
+	// Start HTTP server.
+	srv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      newHTTPHandler(),
+		WriteTimeout: 10 * time.Second,
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+	}
 
-			for name, value := range getTracingHeaders(r) {
-				req.Header.Set(name, value)
-			}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
 
-			upstreamResponse, err := client.Do(req)
-			if err != nil {
-				fmt.Fprintf(w, "Error contacting upstream service: %v", err)
-				return
-			}
-			defer upstreamResponse.Body.Close()
-			body, err := ioutil.ReadAll(upstreamResponse.Body)
-			if err != nil {
-				fmt.Fprintf(w, "Error reading upstream response: %v", err)
-				return
-			}
-			response += fmt.Sprintf(" -> %s", string(body))
+	// Wait for interruption.
+	select {
+	case err = <-srvErr:
+		// Error when starting HTTP server.
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
+
+	// When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
+	err = srv.Shutdown(context.Background())
+	return
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	// handleFunc is a replacement for mux.HandleFunc
+	// which enriches the handler's HTTP instrumentation with the pattern as the http.route.
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		// Configure the "http.route" for the HTTP instrumentation.
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
+	// Register handlers.
+	handleFunc("/", handle)
+
+	// Add HTTP instrumentation for the whole server.
+	handler := otelhttp.NewHandler(mux, "/")
+	return handler
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	ip, hostname := getIPAndHostname()
+	response := fmt.Sprintf("%s(version: %s, ip: %s, hostname: %s)", appName, version, ip, hostname)
+
+	for k, v := range r.Header {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	if upstream != "" {
+		client := &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
+		req, _ := http.NewRequestWithContext(r.Context(), "GET", upstream, nil)
+
+		for name, value := range getTracingHeaders(r) {
+			req.Header.Set(name, value)
 		}
 
-		setHeaders(w, r)
-		fmt.Fprintf(w, response)
-	})
+		upstreamResponse, err := client.Do(req)
+		if err != nil {
+			fmt.Fprintf(w, "Error contacting upstream service: %v", err)
+			return
+		}
+		defer upstreamResponse.Body.Close()
+		body, err := ioutil.ReadAll(upstreamResponse.Body)
+		if err != nil {
+			fmt.Fprintf(w, "Error reading upstream response: %v", err)
+			return
+		}
+		response += fmt.Sprintf(" -> %s", string(body))
+	}
 
-	http.ListenAndServe(":"+port, nil)
+	setHeaders(w, r)
+	fmt.Fprintf(w, response)
 }
 
 func getIPAndHostname() (string, string) {
@@ -87,7 +153,7 @@ func setHeaders(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTracingHeaderKeys() []string {
-	return []string{"X-Ot-Span-Context", "X-Request-Id", "uber-trace-id", "x-b3-traceid", "x-b3-spanid", "x-b3-parentspanid"}
+	return []string{"X-Ot-Span-Context", "Traceparent", "X-Request-Id", "uber-trace-id", "x-b3-traceid", "x-b3-spanid", "x-b3-parentspanid"}
 }
 
 func getTracingHeaders(r *http.Request) map[string]string {
---
+++ otel.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"os"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+)
+
+const OtelExporterOTLPEndpoints = "OTEL_EXPORTER_OTLP_ENDPOINT"
+
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context, serviceName, serviceVersion string) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	if os.Getenv(OtelExporterOTLPEndpoints) == "" {
+		fmt.Println("OTEL_EXPORTER_OTLP_ENDPOINT not provided, skip!!!")
+		return
+	}
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up resource.
+	res, err := newResource(serviceName, serviceVersion)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider(res)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	meterProvider, err := newMeterProvider(res)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	return
+}
+
+func newResource(serviceName, serviceVersion string) (*resource.Resource, error) {
+	return resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+			semconv.ServiceVersion(serviceVersion),
+		))
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider(res *resource.Resource) (*trace.TracerProvider, error) {
+	traceExporter, err := newTraceExporter()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace provider: %w", err)
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	return tracerProvider, nil
+}
+
+func newTraceExporter() (*otlptrace.Exporter, error) {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	traceExporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpoint(os.Getenv(OtelExporterOTLPEndpoints)), otlptracehttp.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+	return traceExporter, nil
+}
+
+func newMeterProvider(res *resource.Resource) (*metric.MeterProvider, error) {
+	metricExporter, err := newMetricExporter()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create meter provider: %w", err)
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(res),
+		metric.WithReader(metric.NewPeriodicReader(metricExporter,
+			// Default is 1m. Set to 5s for demonstrative purposes.
+			metric.WithInterval(5*time.Second))),
+	)
+	return meterProvider, nil
+}
+
+func newMetricExporter() (*otlpmetrichttp.Exporter, error) {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	metricExporter, err := otlpmetrichttp.New(ctx, otlpmetrichttp.WithEndpoint(os.Getenv(OtelExporterOTLPEndpoints)), otlpmetrichttp.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create metrics exporter: %w", err)
+	}
+
+	return metricExporter, nil
+}
